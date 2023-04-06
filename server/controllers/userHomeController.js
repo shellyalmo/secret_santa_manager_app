8f688b4a09cc9ab6f0e8116a0ac6de0c79c7f3e7
shellyalmo <shellyalmo@gmail.com>
2023-04-06 20:58:20 +0300

    feat: join game connect to db

--- server/controllers/userHomeController.js
+++ server/controllers/userHomeController.js
@@ -1,5 +1,6 @@
 import ErrorResponse from "../utils/errorResponse.js";
 import asyncHandler from "../middleware/asyncHandler.js";
+import Game from "../models/Game.js";
 
 // @desc    Get all games per user
 // @route   GET /api/v1/user
@@ -16,7 +17,15 @@ export const getGamesPerUser = asyncHandler(async (req, res, next) => {
 // @route   PUT /api/v1/user
 // @access  Private/User
 export const joinGame = asyncHandler(async (req, res, next) => {
+  await Game.findOneAndUpdate(
+    { id: req.body.gameId },
+    { $push: { users: req.user.id } }
+    // push isn't working for a different user than the admin
+  );
+
   res.status(201).json({
     success: true,
+
+    url: `/user/game/${req.body.gameId}`,
   });
 });
