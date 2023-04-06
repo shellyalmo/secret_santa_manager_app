edf1ad292680c2f0712331f09ee3c8a68993aa7a
Shelly Almoznino <33236921+shellyalmo@users.noreply.github.com>
2023-04-06 18:19:34 +0300

    fix: join game (#6)

--- server/controllers/userHomeController.js
+++ server/controllers/userHomeController.js
@@ -2,7 +2,7 @@ import ErrorResponse from "../utils/errorResponse.js";
 import asyncHandler from "../middleware/asyncHandler.js";
 
 // @desc    Get all games per user
-// @route   GET /api/v1//user
+// @route   GET /api/v1/user
 // @access  Private/User
 export const getGamesPerUser = asyncHandler(async (req, res, next) => {
   res.status(200).json([
@@ -10,4 +10,13 @@ export const getGamesPerUser = asyncHandler(async (req, res, next) => {
     { name: "eid now", theme: "eid", admin: true, id: 2 },
     { name: "purim schmurim", theme: "purim", admin: false, id: 3 },
   ]);
+});
+
+// @desc    join a game
+// @route   PUT /api/v1/user
+// @access  Private/User
+export const joinGame = asyncHandler(async (req, res, next) => {
+  res.status(201).json({
+    success: true,
+  });
 });
