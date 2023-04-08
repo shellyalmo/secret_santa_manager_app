d58dc27af3e1e89364446159b6369b7d5eaa824d
shellyalmo <shellyalmo@gmail.com>
2023-04-08 17:18:28 +0300

    feat: update UI based on if the game isStarted on DB

--- client/src/pages/userPages/UserCurrentGame.jsx
+++ client/src/pages/userPages/UserCurrentGame.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import Gifts from "../../components/user/Gifts";
+import { useParams } from "react-router-dom";
+import useAxios from "../../hooks/useAxios";
+
 const UserCurrentGame = () => {
-  const gameStarted = true; //todo- connect to db and not hardcode here
+  const { id } = useParams();
+  const { data: userGames, error } = useAxios("/user");
+  const result = userGames?.find((game) => game.id === id);
+  const gameStarted = result?.isStarted;
+
   return (
     <>
       {gameStarted ? <Gifts /> : <div>Wait for admin to start the game!</div>}
