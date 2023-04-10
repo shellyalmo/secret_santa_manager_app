f1e143b15e874e0ae4eae84c0fad426c832880b7
shellyalmo <shellyalmo@gmail.com>
2023-04-10 19:45:48 +0300

    feat: display goodbye msg after game is finished

--- client/src/pages/userPages/UserCurrentGame.jsx
+++ client/src/pages/userPages/UserCurrentGame.jsx
@@ -8,10 +8,24 @@ const UserCurrentGame = () => {
   const { data: userGames, error } = useAxios("/user");
   const result = userGames?.find((game) => game.id === id);
   const gameStarted = result?.isStarted;
+  const { data: currentUser } = useAxios("/auth/current-user");
 
+  const currentUserFinishedAssignments = userGames?.some((game) =>
+    game.assignments.some(
+      (assignment) =>
+        assignment.giver === currentUser.data.id && assignment.finished
+    )
+  );
+  console.log(currentUserFinishedAssignments);
   return (
     <>
-      {gameStarted ? <Gifts /> : <div>Wait for admin to start the game!</div>}
+      {gameStarted && !currentUserFinishedAssignments ? (
+        <Gifts />
+      ) : currentUserFinishedAssignments ? (
+        <div>Thanks for playing, bye!</div>
+      ) : (
+        <div>Wait for admin to start the game!</div>
+      )}
     </>
   );
 };
