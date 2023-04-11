994a3809ec4a38e074ced63e66dc0c656c9f90fa
shellyalmo <shellyalmo@gmail.com>
2023-04-11 13:04:18 +0300

    fix: show "thanks for playing" msg only if user finished giving gift

--- client/src/pages/userPages/UserCurrentGame.jsx
+++ client/src/pages/userPages/UserCurrentGame.jsx
@@ -10,18 +10,16 @@ const UserCurrentGame = () => {
   const gameStarted = result?.isStarted;
   const { data: currentUser } = useAxios("/auth/current-user");
 
-  const currentUserFinishedAssignments = userGames?.some((game) =>
-    game.assignments.some(
-      (assignment) =>
-        assignment.giver === currentUser.data.id && assignment.finished
-    )
+  const currentUserFinishedAssignments = result?.assignments?.some(
+    (assignment) =>
+      assignment.giver === currentUser.data.id && assignment.finished
   );
-  console.log(currentUserFinishedAssignments);
+
   return (
     <>
       {gameStarted && !currentUserFinishedAssignments ? (
         <Gifts />
-      ) : currentUserFinishedAssignments ? (
+      ) : gameStarted && currentUserFinishedAssignments ? (
         <div>Thanks for playing, bye!</div>
       ) : (
         <div>Wait for admin to start the game!</div>
